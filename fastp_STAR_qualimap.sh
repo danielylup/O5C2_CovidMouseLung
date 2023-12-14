d5f333b465f59ec616790e1d0a2aad969ed630a3
danielylup <127704531+danielylup@users.noreply.github.com>
2023-12-14 10:40:15 +0800

    Create fastp_STAR_qualimap.sh
    Bash script to process all four bulk RNA-seq dataset

---
+++ fastp_STAR_qualimap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+INPUT_PATH = "~/Project/nx107"
+SAMPLES = "GG01_S1 GG03_S3 GG05_S5 GG06_S6"
+
+for SAMPLE in ${SAMPLES}; do
+
+fastp -w 16 -i ${INPUT_PATH}/Sample_${SAMPLE}/LTT23_${SAMPLE}_L001_R1_001.fastq.gz \
+      -o ${INPUT_PATH}/QC_file/LTT23_${SAMPLE}_L001_R1.filtered.fastq.gz \
+      -h ${INPUT_PATH}/QC_file/QC_report/LTT23_${SAMPLE}.html \
+      -j ${INPUT_PATH}/QC_file/QC_report/LTT23_${SAMPLE}.json
+
+STAR --runThreadN 56 \
+     --readFilesCommand zcat
+     --genomeDir ~/Project/Reference/Mmus/Ref/STAR/Mmus_GRCm39/ \
+     --outFileNamePrefix ${INPUT_PATH}/STAR_output/${SAMPLE}_STAR \
+     --readFilesIn ${INPUT_PATH}/QC_file/LTT23_${SAMPLE}_L001_R1.filtered.fastq.gz
+
+samtools view -@ 56 -b ${INPUT_PATH}/STAR_output/${SAMPLE}_STAR/${SAMPLE}_STARAligned.out.sam | samtools sort -@ 56 -o ${INPUT_PATH}/STAR_output/${SAMPLE}_STAR/${SAMPLE}_sorted.bam
+
+qualimap rnaseq -bam ${INPUT_PATH}/STAR_output/${SAMPLE}_STAR/LTT23_GG01_sorted.bam \
+                -gtf ~/Project/Reference/Mmus/data/GCF_000001635.27/GCF_000001635.27_GRCm39_genomic.gtf \
+                -outdir ${INPUT_PATH}/QC_file/qualimap_output/${SAMPLE}/ \
+                -s --java-mem-size=56G
+
+done
